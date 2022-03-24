045fa13f7234815f65523642d2de0bff9ff874d7
cosmin ovreiu <ovreiu.cosmin@yahoo.ro>
2022-03-24 23:01:07 +0200

    Add project files.

---
+++ 99vehicles_CosminOvreiu/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace _99vehicles_CosminOvreiu
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
